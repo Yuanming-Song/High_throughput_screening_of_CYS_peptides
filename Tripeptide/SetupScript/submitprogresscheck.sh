5c21a24a5ad73d98f3ffac1c7b213d238adf0d38
Yuanming Song <yuanmis1@hpc3-19-15.local>
2025-03-20 12:03:57 -0700

    Stop tracking all files

--- Tripeptide/SetupScript/submitprogresscheck.sh
+++
@@ -1,9 +0,0 @@
-sbatch --job-name="CG Sim Check" \
-               --mail-user=yuanmis1@uci.edu --mail-type=FAIL \
-               --account=dtobias_lab \
-               --partition=standard \
-               --nodes=1 \
-               --ntasks-per-node=1 \
-               --cpus-per-task=1 \
-               --time=24:00:00 \
-               --wrap="bash /dfs9/tw/yuanmis1/mrsec/ML-MD-Peptide/checkprogress.sh"
